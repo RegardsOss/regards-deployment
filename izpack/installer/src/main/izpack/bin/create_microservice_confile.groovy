68aae86ac050b8d4e3eb65bd6fa9b3f2815755a4
xbrochar <xavier-alexandre.brochard@c-s.fr>
2017-04-06 18:32:21 +0200

    Add validation on groovy cli options.

--- izpack/installer/src/main/izpack/bin/create_microservice_confile.groovy
+++ izpack/installer/src/main/izpack/bin/create_microservice_confile.groovy
@@ -1,25 +1,28 @@
 #!/usr/bin/env groovy
 
-def cli = new CliBuilder(usage: 'install_registry.groovy [-h] -i <regards install directory> -c <configuration string> -t <microservice type>')
+def cli = new CliBuilder(usage: 'create_microservice_confile.groovy [-h] -i <regards install directory> -c <configuration string> -t <microservice type>')
+
 // Create the list of options.
 cli.with {
   h longOpt: 'help', 'Show usage information'
-  i longOpt: 'regards-installdir', args: 1, argName: 'installdir', 'regards install directory'
-  c longOpt: 'regards-microservice-config', args: 1, argName: 'microservice-config', 'configuration string'
-  t longOpt: 'regards-microservice-type', args: 1, argName: 'microservice-type', 'microservice type'
+  i longOpt: 'regards-installdir', args: 1, argName: 'installdir', 'regards install directory', required: true
+  c longOpt: 'regards-microservice-config', args: 1, argName: 'microservice-config', 'configuration string', required: true
+  t longOpt: 'regards-microservice-type', args: 1, argName: 'microservice-type', 'microservice type', required: true
 }
 
+// Retrieve options
 def options = cli.parse(args)
-if (!options) {
-  return
-}
+
+// End if options are incorrect
+if (!options) return
+
 // Show usage text when -h or --help option is used.
 if (options.h) {
   cli.usage()
   return
 }
 
-// TODO GBN : Faire la lecture précise des paramètres et notamment les asserts pour vérifier les paramètres 
+// Parse options 
 String regardsInstallDir = options.i;
 String xmlConfig = options.c;
 String microserviceType = options.t;
