b9ad9595bdd72866b0c68ed8ef2fadacfaca9e0b
sbinda <sebastien.binda@c-s.fr>
2019-11-05 17:50:28 +0100

    Use of G1 garbage collector for microservices

--- izpack/installer/src/main/izpack/bin/start_microservice.sh
+++ izpack/installer/src/main/izpack/bin/start_microservice.sh
@@ -126,9 +126,9 @@ do
 
     if [ ${MICROSERVICE_TYPE} == "frontend" ]
     then
-        java -Xms${microservices_infos_t[xmx]} -Xmx${microservices_infos_t[xmx]} -jar -Dserver.address="0.0.0.0"  -Dserver.port="${microservices_infos_t[port]}" ${lib_exec_java} --regards.frontend.www.path=./www > "${log_file}" 2>&1 &
+        java -XX:+UseG1GC -Xms${microservices_infos_t[xmx]} -Xmx${microservices_infos_t[xmx]} -jar -Dserver.address="0.0.0.0"  -Dserver.port="${microservices_infos_t[port]}" ${lib_exec_java} --regards.frontend.www.path=./www > "${log_file}" 2>&1 &
     else
-        java -Xms${microservices_infos_t[xmx]} -Xmx${microservices_infos_t[xmx]} -Dserver.address="0.0.0.0" -Dserver.port="${microservices_infos_t[port]}" -Dloader.path=${plugins_dir} -cp ${microservice_conf_dir} -Dlogging.config=${logback_file} -jar ${lib_exec_java}  > "${log_file}" 2>&1 &
+        java -XX:+UseG1GC -Xms${microservices_infos_t[xmx]} -Xmx${microservices_infos_t[xmx]} -Dserver.address="0.0.0.0" -Dserver.port="${microservices_infos_t[port]}" -Dloader.path=${plugins_dir} -cp ${microservice_conf_dir} -Dlogging.config=${logback_file} -jar ${lib_exec_java}  > "${log_file}" 2>&1 &
     fi
     pid=$!
 
